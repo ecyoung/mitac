1d39e95e751f0718e2d71616bbaab27153f6bca9
陳敬尹YINCHEN <yinchen@mitac.com.tw>
2022-12-19 15:45:59 +0800

    add decision1 with mlflow logs

--- UI-ML-V2-prophet/util/font_manager.py
+++
@@ -1,9 +0,0 @@
-
-import matplotlib.font_manager as font_manager
-import streamlit as st
-
-
-def get_font():
-    if 'font' not in st.session_state:
-        st.session_state['font'] = font_manager.FontProperties(fname=st.session_state['lang_config']['font_path'])
-    return st.session_state['font']
