4fa316d0d5927fac393174d8959e49cd7ccc20ac
陳敬尹YINCHEN <yinchen@mitac.com.tw>
2022-12-28 09:43:34 +0800

    forgot to switch the txt

--- UI-ML-V2-prophet-all/UI-ML-V2-prophet/app_pages/Classification/clf1.py
+++ UI-ML-V2-prophet-all/UI-ML-V2-prophet/app_pages/Classification/clf1.py
@@ -243,7 +243,7 @@ def app():
             # Get current time for run name
             nameForRunName = str(datetime.now())
             with mlflow.start_run(run_name=nameForRunName):
-                mlflow.set_tag("mlflow.user", "YinCehn")
+                mlflow.set_tag("mlflow.user", get_user())
                 mlflow.log_params(paramdist)
                 mlflow.sklearn.log_model(model, "model", registered_model_name='Classification')
                 mlflow.log_metrics(metricsdict)
